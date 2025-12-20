de996541ec1948b8f8ebc4698159bc5069dd9f24
Simon <bramkampsimon@yahoo.de>
2025-12-20 12:10:05 +0100

    added paladin talking

--- Adventure.py
+++ Adventure.py
@@ -1,7 +1,12 @@
+import random
+
+# Welcome messege to the Adventure
 print("The wind carries the echo of a forgotten era through the narrow streets as the sun slowly sinks behind the jagged peaks.")
 print("You stand on the threshold of a place that doesn't appear on any map.")
 print("In your pocket rests an ancient parchment, its seal broken only this morning—and with it, the silence of the last hundred years.")
 print("Your heartbeat sets the pace: there is no turning back.")
+
+number_of_classes = 5 # Hero classes you to play with
 
 player_name = input("Whats your name?")
 print(player_name + ", welcome to an epic adventure full of mysteries and danger")
@@ -11,31 +16,44 @@ Hero_class2 = 2
 Hero_class3 = 3
 Hero_class4 = 4
 
-print("Please choose your class for this adventure.")
+print("Please choose your class for this adventure.") # Choose your Class you wanna play with
 print("1. Paladin")
 print("2. Barbarian")
 print("3. Cleric")
 print("4. Mage")
-print("|_| |_| |_| |_|")
+print("|_|" * number_of_classes)
 
 guessed_Hero_class = input("Please select your Class.")
 guessed_Hero_class = int(guessed_Hero_class)
 
-if guessed_Hero_class ==Hero_class1:
+if guessed_Hero_class ==Hero_class1: # Paladin
     print("Youve choosen the Paladin.")
     print("A paladin is a melee fighter who wears heavy armor (plate) and uses divine powers to support allies or punish enemies.")
+    print("Your Adventure starts in a small Tavern in a town called Ardea")
+    print("Next to your table u see a man coverd in a dark green cloak")
+    print("Talk to him? 1 yes. 2 no.")
+    green_cloak1 = 1
+    green_cloak2 = 2
+    guessed_green_cloak = input("Talk to him`?")
+    guessed_green_cloak = int(guessed_green_cloak)
+    if guessed_green_cloak == green_cloak1:
+        print("Greetings stranger")
+    if guessed_green_cloak == green_cloak2:
+        print("You stay at your table, listening to the music the bard is playing")    
 
-if guessed_Hero_class == Hero_class2:
+if guessed_Hero_class == Hero_class2: # Barbarian
     print("Youve choosen the Barbarian.") 
     print("In contrast to the paladin, who is guided by moral oaths and divine magic, the barbarian follows his animal instincts and the primal force of his rage.")
 
-if guessed_Hero_class == Hero_class3:
+if guessed_Hero_class == Hero_class3: # Cleric
     print("Youve choosen the Cleric.")    
     print("The cleric is a divine magician who acts as an intermediary between the mortal world and the gods.")
-if guessed_Hero_class == Hero_class4:
+
+if guessed_Hero_class == Hero_class4: # Mage
     print("Youve choosen the Mage")   
     print("The mage (often also spell wizard or mage) is the epitome of the intellectual magic user who manipulates reality through years of study and arcane arts.")   
     
+
 
 
 
